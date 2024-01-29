1e48f93d71b87e590ec3e334a64bd3ce7a16038c
hebong79 <hebong2@gmail.com>
2024-01-29 16:06:25 +0900

    UserInfo 수정

--- NetLobby/src/UserInfo.ts
+++ NetLobby/src/UserInfo.ts
@@ -1,34 +1,30 @@
 //서버에 연결된 유정 기본 정보
 export class UserInfo {
-    constructor( 
-                  public userId:string = "",        // 유저 id
-                  public socketId:string ="",     // socket.io에서 지정한 socket id 
-                  public ip : string = "",         // 로컬 IP
-                  public publicIp : string = "",   // 공용 IP
-                  public dataPort: number = 0,    // Data event port
-                  public movePort: number = 0)    // 이동 전용 port
-    {
-      this.userId = userId;
-      this.socketId = socketId;
-      this.ip = ip;
-      this.publicIp = publicIp;
-      this.dataPort = dataPort;
-      this.movePort = movePort;
-    }
-
-    SetInfo( info : UserInfo) : void {
-      this.userId = info.userId;
-      this.socketId = info.socketId;
-      this.ip = info.ip;              
-      this.publicIp = info.publicIp; 
-      this.dataPort = info.dataPort;
-      this.movePort = info.movePort;
-    }
+  constructor(
+    public userId: string = '', // 유저 id
+    public socketId: string = '', // socket.io에서 지정한 socket id
+    public ip: string = '', // 로컬 IP
+    public publicIp: string = '', // 공용 IP
+    public dataPort: number = 0, // Data event port
+    public movePort: number = 0 // 이동 전용 port
+  ) {
+    this.userId = userId;
+    this.socketId = socketId;
+    this.ip = ip;
+    this.publicIp = publicIp;
+    this.dataPort = dataPort;
+    this.movePort = movePort;
   }
 
+  SetInfo(info: UserInfo): void {
+    this.userId = info.userId;
+    this.socketId = info.socketId;
+    this.ip = info.ip;
+    this.publicIp = info.publicIp;
+    this.dataPort = info.dataPort;
+    this.movePort = info.movePort;
+  }
+}
 
- 
-
-
-  export default UserInfo;
-  //module.exports = UserInfo;
+export default UserInfo;
+//module.exports = UserInfo;
